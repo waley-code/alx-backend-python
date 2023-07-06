87eb6e503b4c4375231931f904b550b57ee32ce8
waley-code <oyewaleadeosun@yahoo.com>
2023-07-06 20:05:33 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""type-annotated function to_str that takes a
+    float n as argument and treturns the string
+    representation of the floa.
+"""
+
+
+def to_str(n: float) -> str:
+    """returns string
+        representation of the floa.
+    """
+    return str(n)
