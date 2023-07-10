353c42b9de8a9c52f89efce1a16eb8dcb1a16421
waley-code <oyewaleadeosun@yahoo.com>
2023-07-10 22:06:14 +0100

    completed task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Test file for pait_n coroutine
+'''
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+
+def measure_time(n, max_delay):
+    """returns total_time / n. """
+    start_time = time.time()
+
+    async def main():
+        await wait_n(n, max_delay)
+
+    asyncio.run(main())
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    average_time = total_time / n
+
+    return average_time
