90da6951435bb53c23c24d9a5ebdde6aa5936345
waley-code <oyewaleadeosun@yahoo.com>
2023-07-11 08:50:21 +0100

    fixed task 3 & 4 with anotation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,7 +8,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """takes an integer max_delay and
         returns a asyncio.Task
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n, max_delay):
+async def task_wait_n(n: int, max_delay: int) -> float:
     """wait_n except task_wait_random
         is
     """
