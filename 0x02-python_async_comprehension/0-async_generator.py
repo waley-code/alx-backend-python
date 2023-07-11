6c642c2aa5e8ccb7c1e30aecdc6dffdd7451eef4
waley-code <oyewaleadeosun@yahoo.com>
2023-07-11 11:48:14 +0100

    completest task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async_generator() async function"""
+
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.List[float]:
+    """async_generator() async function"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
